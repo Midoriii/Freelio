1b60b1f9c786c86168c58249602a8bc2086f1455
Pavel Borčin <xborcin1@fi.muni.cz>
2020-11-02 22:21:19 +0100

    added basic tabbed pane

--- src/main/java/pv168/freelancer/ui/MainWindow.java
+++ src/main/java/pv168/freelancer/ui/MainWindow.java
@@ -5,12 +5,13 @@ import java.awt.*;
 
 public class MainWindow {
     private JFrame frame;
-    private JPanel panel;
+//    private JPanel panel;
     private CardLayout cardLayout;
+    private JTabbedPane tabbedPane;
 
     public MainWindow() {
         frame = createFrame();
-        panel = new JPanel();
+        tabbedPane = createTabbedPane();
     }
 
     public void show() {
@@ -18,9 +19,28 @@ public class MainWindow {
     }
 
     private JFrame createFrame() {
-        var frame = new JFrame("Work evidence");
+        var frame = new JFrame("Invoice table");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         return frame;
+    }
+
+    private JTabbedPane createTabbedPane() {
+        JTabbedPane tabbedPane = new JTabbedPane();
+
+        JPanel invoices = new JPanel();
+        JLabel label1 = new JLabel("invoices");
+        invoices.add(label1);
+
+        JPanel workDone = new JPanel();
+
+        JPanel profitCalc = new JPanel();
+
+        tabbedPane.addTab("Invoices", invoices);
+        tabbedPane.addTab("Work done", workDone);
+        tabbedPane.addTab("Profit calculator", profitCalc);
+
+        frame.getContentPane().add(tabbedPane, BorderLayout.CENTER);
+        return tabbedPane;
     }
 
 }
