0b7f30e6b904f198e697d7b2eb958c808911457c
xkopeck5 <xkopeck5@fi.muni.cz>
2020-11-10 19:16:22 +0100

    Lorem ipsum

--- src/main/java/pv168/freelancer/data/TestDataGenerator.java
+++ src/main/java/pv168/freelancer/data/TestDataGenerator.java
@@ -21,6 +21,30 @@ public class TestDataGenerator {
     private static final List<String> WORK_NAMES = List.of("UI Design", "Training Pokemon",
     "UX Lecturing", "Recording Despacito 2", "Updating Windows", "Making coffee", "Repairing coffee machine");
 
+    private static final List<String> DESCRIPTIONS = List.of(
+            "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Sed elit dui, pellentesque a, faucibus vel, interdum nec, diam.",
+            "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
+            "Sed convallis magna eu sem.",
+            "Nulla non lectus sed nisl molestie malesuada.",
+            "In rutrum.",
+            "Fusce dui leo, imperdiet in, aliquam sit amet, feugiat eu, orci.",
+            "Duis risus.",
+            "Proin mattis lacinia justo. Nunc auctor.",
+            "Morbi imperdiet, mauris ac auctor dictum, nisl ligula egestas nulla, et sollicitudin sem purus in lacus. Maecenas sollicitudin. ",
+            "Etiam dui sem, fermentum vitae, sagittis id, malesuada in, quam.");
+
+    private static final List<String> NOTES = List.of(
+            "Praesent in mauris.",
+            "Etiam ligula.",
+            "Phasellus et lorem.",
+            "Donec iaculis gravida nulla.",
+            "Nullam justo enim.",
+            "Aenean fermentum risus id tortor.",
+            "Duis risus.",
+            "Phasellus enim erat.",
+            "Sed ac dolor.",
+            "Maecenas aliquet.");
+
     private static final LocalDate MIN_WORK = LocalDate.of(2017, JANUARY, 1);
     private static final LocalDate MAX_WORK = LocalDate.of(2020, DECEMBER, 31);
 
@@ -28,7 +52,7 @@ public class TestDataGenerator {
 
     public WorkType createTestWorkType() {
         String name = selectRandom(WORK_NAMES);
-        String description = "";
+        String description = selectRandom(DESCRIPTIONS);
         double hourlyRate = Math.random() * 96 + 5;
         return new WorkType(name, hourlyRate, description);
     }
@@ -56,7 +80,7 @@ public class TestDataGenerator {
         LocalDateTime workStart = getRandomDateTime();
         LocalDateTime workEnd = getRandomDateTime(workStart);
         WorkType workType = createTestWorkType();
-        String note = "";
+        String note = selectRandom(NOTES);
         return new WorkDone( workStart, workEnd, workType, note);
     }
 
