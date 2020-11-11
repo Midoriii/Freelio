bd39b8d91a28bf5aae624a7ba56d900383bc53dd
xkopeck5 <xkopeck5@fi.muni.cz>
2020-11-11 14:57:30 +0100

    10 -> 100

--- src/main/java/pv168/freelancer/ui/cards/WorkDoneCard.java
+++ src/main/java/pv168/freelancer/ui/cards/WorkDoneCard.java
@@ -1,6 +1,5 @@
 package pv168.freelancer.ui.cards;
 
-
 import pv168.freelancer.data.TestDataGenerator;
 import pv168.freelancer.model.WorkDone;
 import pv168.freelancer.ui.WorkDoneDetail;
@@ -40,10 +39,10 @@ public class WorkDoneCard extends Card {
         setUpGroupLayout();
     }
 
-    private Object[][] createWorkTable(List<WorkDone> worksDone) {
-        Object[][] result = new Object[10][];
+    private Object[][] createWorkTable(List<WorkDone> worksDone, int count) {
+        Object[][] result = new Object[count][];
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < count; i++) {
             ArrayList<String>tuple = new ArrayList<>();
                 tuple.add(worksDone.get(i).getWorkType().getName());
                 tuple.add(worksDone.get(i).getWorkStart().toString());
@@ -57,7 +56,7 @@ public class WorkDoneCard extends Card {
     private void setUpTable() {
         TestDataGenerator testDataGenerator = new TestDataGenerator();
         workDoneTable = new JTable(new DefaultTableModel(
-                createWorkTable(testDataGenerator.createTestData(10)),
+                createWorkTable(testDataGenerator.createTestData(100), 100),
                    new String[]{"Work Type", "From", "To", "Expected Pay"}
         ));
     }
