205cc622460fc529317b213846ccc893efceb551
Pavel Borčin <xborcin1@fi.muni.cz>
2020-11-13 10:19:05 +0100

    worktypedetail layout

--- src/main/java/pv168/freelancer/ui/WorkTypeDetail.java
+++ src/main/java/pv168/freelancer/ui/WorkTypeDetail.java
@@ -11,6 +11,11 @@ public class WorkTypeDetail extends JDialog {
     private JPanel quitPanel;
     private JPanel contentPanel;
 
+    private JTextField nameField;
+    private JTextField hourlyRateField;
+    private JTextArea descriptionArea;
+
+
     private final ComponentMover cm = new ComponentMover();
 
     public WorkTypeDetail(JFrame owner, Boolean modality){
@@ -29,11 +34,68 @@ public class WorkTypeDetail extends JDialog {
         setVisible(true);
     }
 
+    private void addNameField(GridBagConstraints gbc) {
+        JLabel name = new JLabel("Name:");
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        contentPanel.add(name, gbc);
+        nameField = new JTextField(20);
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        contentPanel.add(nameField, gbc);
+    }
+
+    private void addHourlyRateField(GridBagConstraints gbc) {
+        JLabel hourlyRate = new JLabel("Hourly rate:");
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        contentPanel.add(hourlyRate, gbc);
+        hourlyRateField = new JTextField(8);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        contentPanel.add(hourlyRateField, gbc);
+    }
+
+    private void addDescriptionArea(GridBagConstraints gbc) {
+        JLabel description = new JLabel("Description:");
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        contentPanel.add(description, gbc);
+        descriptionArea = new JTextArea(5, 20);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+        contentPanel.add(descriptionArea, gbc);
+    }
+
+    private void addConfirmButton(GridBagConstraints gbc) {
+        JButton okBtn = new JButton("OK");
+        okBtn.addActionListener(e -> dispose());
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        gbc.insets = new Insets(20, 150, 20, 150);
+        gbc.gridwidth = 2;
+        contentPanel.add(okBtn, gbc);
+    }
+
     private void setUpContentPanel() {
-        JLabel testText = new JLabel("Ha!");
         contentPanel = new JPanel();
+        contentPanel.setLayout(new GridBagLayout());
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(20, 10, 20, 10);
+        gbc.anchor = GridBagConstraints.PAGE_START;
+
         contentPanel.setPreferredSize(new Dimension(450, 540));
-        contentPanel.add(testText);
+        addNameField(gbc);
+        addHourlyRateField(gbc);
+        addDescriptionArea(gbc);
+        addConfirmButton(gbc);
     }
 
     private void setUpQuitPanel(JFrame owner) {
