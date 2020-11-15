3735fc35e8694fd17889914b49f51a5f3287ab2c
Midoriii <stephen.Team24@gmail.com>
2020-11-15 23:54:42 +0100

    delinting

--- src/main/java/pv168/freelancer/ui/utils/Icons.java
+++ src/main/java/pv168/freelancer/ui/utils/Icons.java
@@ -5,7 +5,7 @@ package pv168.freelancer.ui.utils;
 public final class Icons {
 
     public static final CustomIcon MINIMIZE_ICON = createIcon("/minimize.png");
-    public static final CustomIcon QUIT_ICON = createIcon("/quit.png");;
+    public static final CustomIcon QUIT_ICON = createIcon("/quit.png");
 
     private Icons() {
         throw new AssertionError("This class is not instantiable");
