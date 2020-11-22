0456445626084271dbab9da227b19cbee3bea7ba
Pavel Borčin <xborcin1@fi.muni.cz>
2020-11-22 15:25:27 +0100

    database creates tables for WorkDone and WorkType

--- src/main/java/pv168/freelancer/data/WorkDao.java
+++ src/main/java/pv168/freelancer/data/WorkDao.java
@@ -1,5 +1,6 @@
 package pv168.freelancer.data;
 
+
 import javax.sql.DataSource;
 import java.sql.SQLException;
 
@@ -9,36 +10,67 @@ public class WorkDao {
 
     public WorkDao(DataSource dataSource) {
         this.dataSource = dataSource;
-        initTable();
+        initWorkTypeTable();
+        initWorkDoneTable();
     }
 
-    private void initTable() {
-        if (!tableExists("APP", "WORK_DONE")) {
-            createTable();
+    private void initWorkDoneTable() {
+        if (!workDoneTableExists("APP", "WORK_DONE")) {
+            createWorkDoneTable();
         }
     }
 
-    private boolean tableExists(String schemaName, String tableName) {
+    private boolean workDoneTableExists(String schemaName, String tableName) {
         try (var connection = dataSource.getConnection();
              var rs = connection.getMetaData().getTables(null, schemaName, tableName, null)) {
             return rs.next();
         } catch (SQLException ex) {
-            throw new RuntimeException("Failed to detect if the table " + schemaName + "." + tableName + " exist", ex);
+            throw new RuntimeException("Failed to detect if the table " + schemaName + "." + tableName + " exists", ex);
         }
     }
 
-    private void createTable() {
+    private void createWorkDoneTable() {
         try (var connection = dataSource.getConnection();
              var st = connection.createStatement()) {
             st.executeUpdate("CREATE TABLE APP.WORK_DONE (" +
+                    "ID BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY," +
+                    "WT_ID BIGINT REFERENCES WORK_TYPE(ID)," +
                     "WORK_START TIMESTAMP," +
                     "WORK_END TIMESTAMP," +
                     "WORK_TYPE VARCHAR(50)," +
                     "DESCRIPTION VARCHAR(200)" +
                     ")");
         } catch (SQLException ex) {
-            throw new RuntimeException("Failed to create WORK table", ex);
+            throw new RuntimeException("Failed to create WORK_DONE table", ex);
         }
     }
 
+    private void initWorkTypeTable() {
+        if (!workTypeTableExists("APP", "WORK_TYPE")) {
+            createWorkTypeTable();
+        }
+    }
+
+    private boolean workTypeTableExists(String schemaName, String tableName) {
+        try (var connection = dataSource.getConnection();
+             var rs = connection.getMetaData().getTables(null, schemaName, tableName, null)) {
+            return rs.next();
+        } catch (SQLException ex) {
+            throw new RuntimeException("Failed to detect if the table " + schemaName + "." + tableName + " exists", ex);
+        }
+    }
+
+    private void createWorkTypeTable() {
+        try (var connection = dataSource.getConnection();
+             var st = connection.createStatement()) {
+            st.executeUpdate("CREATE TABLE APP.WORK_TYPE (" +
+                    "ID BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY," +
+                    "NAME varchar(100)," +
+                    "HOURLY_RATE FLOAT(24)," +
+                    "DESCRIPTION varchar(200)" +
+                    ")");
+        } catch (SQLException ex) {
+            throw new RuntimeException("Failed to create WORK_TYPE table", ex);
+        }
+    }
 }
