4eaf4760e8e40dd258ad9b89c0f8c6d10777aaaf
Pavel Borčin <xborcin1@fi.muni.cz>
2020-11-22 15:59:02 +0100

    database insert try no.1

--- src/main/java/pv168/freelancer/data/WorkDao.java
+++ src/main/java/pv168/freelancer/data/WorkDao.java
@@ -1,8 +1,12 @@
 package pv168.freelancer.data;
 
 
+import pv168.freelancer.model.WorkDone;
+
 import javax.sql.DataSource;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
 
 public class WorkDao {
 
@@ -12,6 +16,31 @@ public class WorkDao {
         this.dataSource = dataSource;
         initWorkTypeTable();
         initWorkDoneTable();
+    }
+
+    public void create(WorkDone workDone) {
+        try (var connection = dataSource.getConnection();
+
+             var st1 = connection.prepareStatement(
+                     "INSERT INTO WORK_TYPE (NAME, HOURLY_RATE, DESCRIPTION) VALUES (?, ?, ?)",
+                     Statement.RETURN_GENERATED_KEYS)) {
+            st1.setString(1, workDone.getWorkType().getName());
+            st1.setDouble(2, workDone.getWorkType().getHourlyRate());
+            st1.setString(3, workDone.getWorkType().getDescription());
+            st1.executeUpdate();
+            try (var rs = st1.getGeneratedKeys();
+                    var st2 = connection.prepareStatement(
+                    "INSERT INTO WORK_DONE (WT_ID, WORK_START, WORK_END, DESCRIPTION) VALUES (?, ?, ?, ?)")) {
+                st2.setDouble(1, rs.getLong(1));
+                st2.setTimestamp(2, Timestamp.valueOf(workDone.getWorkStart()));
+                st2.setTimestamp(3, Timestamp.valueOf(workDone.getWorkEnd()));
+                st2.setString(4, workDone.getDescription());
+            } catch (SQLException e) {
+                throw new RuntimeException("Failed to store Work Type" + workDone.getWorkType(), e);
+            }
+        } catch (SQLException ex) {
+            throw new RuntimeException("Failed to store Work Done" + workDone, ex);
+        }
     }
 
     private void initWorkDoneTable() {
@@ -37,7 +66,6 @@ public class WorkDao {
                     "WT_ID BIGINT REFERENCES WORK_TYPE(ID)," +
                     "WORK_START TIMESTAMP," +
                     "WORK_END TIMESTAMP," +
-                    "WORK_TYPE VARCHAR(50)," +
                     "DESCRIPTION VARCHAR(200)" +
                     ")");
         } catch (SQLException ex) {
@@ -66,7 +94,7 @@ public class WorkDao {
             st.executeUpdate("CREATE TABLE APP.WORK_TYPE (" +
                     "ID BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY," +
                     "NAME varchar(100)," +
-                    "HOURLY_RATE FLOAT(24)," +
+                    "HOURLY_RATE FLOAT(30)," +
                     "DESCRIPTION varchar(200)" +
                     ")");
         } catch (SQLException ex) {
